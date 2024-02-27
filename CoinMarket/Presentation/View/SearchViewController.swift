38e85af05a6f927ec1fcaa3bb17a47acc9fb4a5a
Jin0331 <sempre813@naver.com>
2024-02-27 21:45:09 +0900

    [Desgin] Base View / Controller 추가

--- CoinMarket/Presentation/View/SearchViewController.swift
+++ CoinMarket/Presentation/View/SearchViewController.swift
@@ -11,23 +11,5 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //trend API
-        CoinAPIManager.shared.callRequest(type: SearchTrendingModel.self, api: .trend) { value, error in
-            
-        dump(value)
-        }
-
-        CoinAPIManager.shared.callRequest(type: SearchModel.self, api: .search(coinName: "bitcoinpow")) { value, error in
-
-            dump(value)
-        }
-        
-        //TODO: - market에서 ids 받을 때, Array -> join 형태로 진행하면 될 듯
-        CoinAPIManager.shared.callRequest(type: MarketCoinModel.self, api: .market(ids: "bitcoinpow")) { value, error in
-
-            dump(value)
-        }
-        
     }
 }
